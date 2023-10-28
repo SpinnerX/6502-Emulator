8f4b8bbf427b0f04a3e2049725d22dbf8ae1c5c6
type <type>
2023-10-27 22:03:23 -0700

    Added more test cases

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -117,7 +117,123 @@ void checkLDYInstructionCorrectnessInit(CPU& cpu){
     };
 }
 
+void checkingSTAInstructionCorrectnessInit(CPU& cpu){
+    BaseRegister* reg1 = cpu.decode<0x85>();
+    BaseRegister* reg2 = cpu.decode<0x95>();
+    BaseRegister* reg3 = cpu.decode<0x8D>();
+    BaseRegister* reg4 = cpu.decode<0x9D>();
+    BaseRegister* reg5 = cpu.decode<0x99>();
+    BaseRegister* reg6 = cpu.decode<0x81>();
+    BaseRegister* reg7 = cpu.decode<0x91>();
 
+    RegisterTraits traitsMock1 = RegisterTraits(RegisterTypes::STA, AddressModes::ZPG, 0x85, 2, 3);
+    RegisterTraits traitsMock2 = RegisterTraits(RegisterTypes::STA, AddressModes::ZPG_X, 0x95, 2, 4);
+    RegisterTraits traitsMock3 = RegisterTraits(RegisterTypes::STA, AddressModes::ABS, 0x8D, 3, 4);
+    RegisterTraits traitsMock4 = RegisterTraits(RegisterTypes::STA, AddressModes::ABSX, 0x9D, 3, 5);
+    RegisterTraits traitsMock5 = RegisterTraits(RegisterTypes::STA, AddressModes::ABSY, 0x99, 3, 5);
+    RegisterTraits traitsMock6 = RegisterTraits(RegisterTypes::STA, AddressModes::INDIRECT_X, 0x81, 2, 6);
+    RegisterTraits traitsMock7 = RegisterTraits(RegisterTypes::STA, AddressModes::INDIRECT_Y, 0x91, 2, 6);
+
+    RegisterTraits actualTraits1 = reg1->data();
+    RegisterTraits actualTraits2 = reg2->data();
+    RegisterTraits actualTraits3 = reg3->data();
+    RegisterTraits actualTraits4 = reg4->data();
+    RegisterTraits actualTraits5 = reg5->data();
+    RegisterTraits actualTraits6 = reg6->data();
+    RegisterTraits actualTraits7 = reg7->data();
+
+
+    "STA_Register_Init_Test"_test = [&]{
+        expect(actualTraits1 == traitsMock1);
+        expect(actualTraits2 == traitsMock2);
+        expect(actualTraits3 == traitsMock3);
+        expect(actualTraits4 == traitsMock4);
+        expect(actualTraits5 == traitsMock5);
+        expect(actualTraits6 == traitsMock6);
+        expect(actualTraits7 == traitsMock7);
+    };
+    
+
+}
+
+void checkingSTXInstructionCorrectnessInit(CPU& cpu){
+    BaseRegister* reg1 = cpu.decode<0x86>();
+    BaseRegister* reg2 = cpu.decode<0x96>();
+    BaseRegister* reg3 = cpu.decode<0x8E>();
+
+    RegisterTraits traitsMock1 = RegisterTraits(RegisterTypes::STX, AddressModes::ZPG, 0x86, 2, 3);
+    RegisterTraits traitsMock2 = RegisterTraits(RegisterTypes::STX, AddressModes::ZPG_Y, 0x96, 2, 4);
+    RegisterTraits traitsMock3 = RegisterTraits(RegisterTypes::STX, AddressModes::ABS, 0x8E, 3, 4);
+
+
+    RegisterTraits actualTraits1 = reg1->data();
+    RegisterTraits actualTraits2 = reg2->data();
+    RegisterTraits actualTraits3 = reg3->data();
+
+    "STX_Register_Init_Test"_test = [&]{
+        expect(actualTraits1 == traitsMock1);
+        expect(actualTraits2 == traitsMock2);
+        expect(actualTraits3 == traitsMock3);
+    };
+}
+
+
+void checkingSTYInstructionCorrectnessInit(CPU& cpu){
+    BaseRegister* reg1 = cpu.decode<0x84>();
+    BaseRegister* reg2 = cpu.decode<0x94>();
+    BaseRegister* reg3 = cpu.decode<0x8C>();
+
+    RegisterTraits traitsMock1 = RegisterTraits(RegisterTypes::STY, AddressModes::ZPG, 0x84, 2, 3);
+    RegisterTraits traitsMock2 = RegisterTraits(RegisterTypes::STY, AddressModes::ZPG_X, 0x94, 2, 4);
+    RegisterTraits traitsMock3 = RegisterTraits(RegisterTypes::STY, AddressModes::ABS, 0x8C, 3, 4);
+
+    RegisterTraits actualTraits1 = reg1->data();
+    RegisterTraits actualTraits2 = reg2->data();
+    RegisterTraits actualTraits3 = reg3->data();
+
+    "STY_Register_Init_Test"_test  = [&]{
+        expect(actualTraits1 == traitsMock1);
+        expect(actualTraits2 == traitsMock2);
+        expect(actualTraits3 == traitsMock3);
+    };
+}
+
+void checking_TransferInstructions_CorrectnessInit(CPU& cpu){
+    // Since these transfer registers only have one address mode corresponding with thhem
+    // then we will contain thhem under one unit test case
+    BaseRegister* reg1 = cpu.decode<0xAA>(); // TAX
+    BaseRegister* reg2 = cpu.decode<0xA8>(); // TAY
+    BaseRegister* reg3 = cpu.decode<0xBA>(); // TSX
+    BaseRegister* reg4 = cpu.decode<0x8A>(); // TXA
+    BaseRegister* reg5 = cpu.decode<0x9A>(); // TXS
+    BaseRegister* reg6 = cpu.decode<0x98>(); // TYA
+
+    RegisterTraits traitsMock1 = RegisterTraits(RegisterTypes::TAX, AddressModes::IMPLIED, 0xAA, 1, 2);
+    RegisterTraits traitsMock2 = RegisterTraits(RegisterTypes::TAY, AddressModes::IMPLIED, 0xA8, 1, 2);
+    RegisterTraits traitsMock3 = RegisterTraits(RegisterTypes::TSX, AddressModes::IMPLIED, 0xBA, 1, 2);
+    RegisterTraits traitsMock4 = RegisterTraits(RegisterTypes::TXA, AddressModes::IMPLIED, 0x8A, 1, 2);
+    RegisterTraits traitsMock5 = RegisterTraits(RegisterTypes::TXS, AddressModes::IMPLIED, 0x9A, 1, 2);
+    RegisterTraits traitsMock6 = RegisterTraits(RegisterTypes::TYA, AddressModes::IMPLIED, 0x98, 1, 2);
+
+    RegisterTraits actualTraits1 = reg1->data();
+    RegisterTraits actualTraits2 = reg2->data();
+    RegisterTraits actualTraits3 = reg3->data();
+    RegisterTraits actualTraits4 = reg4->data();
+    RegisterTraits actualTraits5 = reg5->data();
+    RegisterTraits actualTraits6 = reg6->data();
+
+
+    "Transfer_Registers_Init_Test"_test = [&]{
+        expect(actualTraits1 == traitsMock1);
+        expect(actualTraits2 == traitsMock3);
+        expect(actualTraits3 == traitsMock3);
+        expect(actualTraits4 == traitsMock4);
+        expect(actualTraits5 == traitsMock5);
+        expect(actualTraits6 == traitsMock6);
+    };
+}
+
+/*
 void checkLSRInstructionCorrectnessInit(CPU& cpu){
     BaseRegister* reg1 = cpu.decode<0x4A>();
     BaseRegister* reg2 = cpu.decode<0x46>();
@@ -147,6 +263,7 @@ void checkLSRInstructionCorrectnessInit(CPU& cpu){
     };
 
 }
+*/
 
 int main() {
     CPU cpu;
@@ -154,5 +271,9 @@ int main() {
     checkingLDAInstructionCorrectnessInit(cpu);
     checkLDXInstructionCorrectnessInit(cpu);
     checkLDYInstructionCorrectnessInit(cpu);
-    checkLSRInstructionCorrectnessInit(cpu);
+    checkingSTAInstructionCorrectnessInit(cpu);
+    checkingSTXInstructionCorrectnessInit(cpu);
+    checkingSTYInstructionCorrectnessInit(cpu);
+    checking_TransferInstructions_CorrectnessInit(cpu);
+    // checkLSRInstructionCorrectnessInit(cpu);
 }
