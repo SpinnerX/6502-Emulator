ecf521d5a3bc17de0dd679fd9559628cd470b046
type <type>
2023-11-04 05:44:53 -0700

    Added a single test case to test loading into x register

--- tests/instructionInitializedTest.h
+++ tests/instructionInitializedTest.h
@@ -12,10 +12,10 @@ using namespace boost::ut;
 void testLDAInstruction(CPUConfigs& conf){
     // Testing operations
     BaseInstruction* testInstruction = new Instruction<0xA9>();
-    testInstruction->operation(conf, 2);
+    testInstruction->operation(conf, 2); // testing that we are loading number 2 into accumualator, essentially doing: LDA $2
 
     BaseInstruction* testInstruction2 = new Instruction<0xA2>();
-    testInstruction2->operation(conf, 2);
+    testInstruction2->operation(conf, 2); // testing that we are loading 2 into register x, essentially doing: LDX $2
 
     "LDAInitialized Address Modes"_test = [&]{
         expect(that % conf.ac == 0x02);
