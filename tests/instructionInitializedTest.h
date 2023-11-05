9209c4d2cc282e814c50195778b416e930ebc346
= <=>
2023-11-04 19:14:25 -0700

    Added additional test cases

--- tests/instructionInitializedTest.h
+++ tests/instructionInitializedTest.h
@@ -9,21 +9,38 @@ using namespace boost::ut;
  * 
  * 
 */
-void testLDAInstruction(CPUConfigs& conf){
+void testLoadInstructions(CPUConfigs& conf){
     // Testing operations
-    BaseInstruction* testInstruction = new Instruction<0xA9>();
-    testInstruction->operation(conf, 2); // testing that we are loading number 2 into accumualator, essentially doing: LDA $2
+    BaseInstruction* lda = new Instruction<0xA9>();
+    lda->operation(conf, 2); // testing that we are loading number 2 into accumualator, essentially doing: LDA $2
 
-    BaseInstruction* testInstruction2 = new Instruction<0xA2>();
-    testInstruction2->operation(conf, 2); // testing that we are loading 2 into register x, essentially doing: LDX $2
+    BaseInstruction* ldx = new Instruction<0xA2>();
+    ldx->operation(conf, 2); // testing that we are loading 2 into register x, essentially doing: LDX $2
 
-    BaseInstruction* testInstruction3 = new Instruction<0xA0>();
-    testInstruction3->operation(conf, 5); // testing that we are loading 2 into register y, essentially doing: LDY $5
+    BaseInstruction* ldy = new Instruction<0xA0>();
+    ldy->operation(conf, 5); // testing that we are loading 2 into register y, essentially doing: LDY $5
 
-    "LDAInitialized Address Modes"_test = [&]{
+    "LoadInstructionInitialized Address Modes"_test = [&]{
         expect(that % conf.ac == 0x02);
         expect(that % conf.x == 0x02);
         expect(that % conf.y == 0x05);
     };
 
+}
+
+
+void testStoringInstructions(CPUConfigs& conf){
+    BaseInstruction* sta = new Instruction<0x85>();
+    int value = 2;
+    sta->operation(conf, value); // we are storing value into a specific memory address depending on opcode.
+
+    BaseInstruction* stx = new Instruction<0x86>();
+    int value2 = 10;
+    stx->operation(conf, value2); // storing value2 into the x register
+
+    "StoreInstructionInitialized Address Modes"_test = [&]{
+        expect(that %  conf[0x85] == value);
+        expect(that % conf.x == value2);
+    };
+    
 }
