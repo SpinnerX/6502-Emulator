8e3cbccbe68b053d736e276c53a31943b2f098f1
type <type>
2023-11-08 02:32:13 -0800

    Added clock

---
+++ 6502_emulator/include/common/Clock.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <chrono>
+#include <algorithm>
+#include <cstring>
+
+/**
+ * 
+ * 
+ * Clock
+ * - Acts as the timer
+ * - That allows the pace of specific operations in the emulated processor to be controlled
+ * - CPU Procesors operate according to its clock cycles, with one operation typically carried out per cycle.
+ * - 
+ * 
+ * 
+*/
+
+struct Clock{
+
+    void tick(){}
+
+
+
+};
